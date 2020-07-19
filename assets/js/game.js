79bb099903289be8920c0d463cbae49a97432fb4
mattverdillo <meverdillo@gmail.com>
2020-07-19 15:28:32 -0500

    fixed fight/skip prompt null/blank issue

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,8 +68,21 @@ var fight = function (enemy) {
   }
 };
 
+//funtion to set name
+var getPlayerName = function() {
+  var name = ""; 
+
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?"); 
+  }
+  //ADD LOOP HERE WITH PROMPT AND CONDITION 
+
+  console.log("Your robot's name is" + name); 
+  return name; 
+}; 
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName (), 
   health: 100,
   attack: 10,
   money: 10, 
